8fe93189c0c793d2cc8735027015e0e328f368cb
YannRondeau <rondeau.yann@hotmail.fr>
2020-07-11 16:43:23 +0200

    feat/ ajout component sidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PodcastComponent } from './podcast/podcast.component';
+import { MerchComponent } from './merch/merch.component';
+import { ContactComponent } from './contact/contact.component';
+import { LabelComponent } from './label/label.component';
 
 const routes: Routes = [
-  { path: 'app-home', component: HomeComponent }
+  { path: 'app-home', component: HomeComponent },
+  { path: 'app-about', component: AboutComponent },
+  { path: 'app-podcast', component: PodcastComponent },
+  { path: 'app-merch', component: MerchComponent },
+  { path: 'app-label', component: LabelComponent },
+  { path: 'app-contact', component: ContactComponent },
+  { path: '', redirectTo: '/app-home', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PodcastComponent } from './podcast/podcast.component';
+import { MerchComponent } from './merch/merch.component';
+import { LabelComponent } from './label/label.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    PodcastComponent,
+    MerchComponent,
+    LabelComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
