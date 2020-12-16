e6b90839e5507cdf03f57fe572f5a32199239a6c
YannRondeau <rondeau.yann@hotmail.fr>
2020-12-16 02:13:46 +0100

    /feat merch

--- src/app/pages/merch/merch.component.ts
+++ src/app/pages/merch/merch.component.ts
@@ -9,7 +9,20 @@ export class MerchComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    let thumbnails = document.getElementsByClassName('thumbnail');
+    let activeImages = document.getElementsByClassName('active');
+
+    for (var i=0; i < thumbnails.length; i++) {
+      thumbnails[i].addEventListener('mouseover', function(){
+        if (activeImages.length > 0){
+          activeImages[0].classList.remove('active');
+        }
+
+        this.classList.add('active');
+        (<HTMLImageElement>document.getElementById('featured')).src = this.src;
+      })
+    }
   }
 
 }
