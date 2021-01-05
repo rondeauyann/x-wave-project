0f3059ec4743d8d70eb4f14ee6e45edc9ee99e91
YannRondeau <rondeau.yann@hotmail.fr>
2021-01-05 19:40:09 +0100

    /feat podcast details

--- src/app/pages/podcast-detail/podcast-detail.component.ts
+++ src/app/pages/podcast-detail/podcast-detail.component.ts
@@ -26,6 +26,27 @@ export class PodcastDetailComponent implements OnInit {
   ngOnInit() {
     this.id = Number(this.route.snapshot.paramMap.get('id'));
     this.podcastDetail = this.podcastService.getPodcastDetail(this.id);
+
+    $('img[data-enlargeable]').addClass('img-enlargeable').click(function(){
+      var src = $(this).attr('src');
+      var modal;
+      function removeModal(){ modal.remove(); $('body').off('keyup.modal-close'); }
+      modal = $('<div>').css({
+          background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
+          backgroundSize: 'contain',
+          width:'100%', height:'100%',
+          position:'fixed',
+          zIndex:'10000',
+          top:'0', left:'0',
+          cursor: 'zoom-out'
+      }).click(function(){
+          removeModal();
+      }).appendTo('body');
+      //handling ESC
+      $('body').on('keyup.modal-close', function(e){
+        if(e.key==='Escape'){ removeModal(); }
+      });
+  });
   }
 
   getSoundcloudUrl() {
